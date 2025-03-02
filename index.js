3643b270036ae1bfc68dceaf02ae28e5a06c8583
SPKCHALLA <shanmukhapadmakumar.chall@gmail.com>
2025-03-02 05:36:06 +0530

    added github page

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express')
 
 const app = express()
 
+const githubdata = {
+  "message": "Not Found",
+  "documentation_url": "https://docs.github.com/rest",
+  "status": "404"
+}
+
 const port = 3000
 
 app.get('/', (req, res) => {
@@ -13,6 +19,9 @@ app.get('/login',(req, res) =>{
 })
 app.get('/dashboard',(req, res) =>{
     res.send('<h1>Guddi naa kodakaa!!</h1><h2>Poraaa bayatiki</h2>')
+})
+app.get('/github',(req, res) =>{
+    res.json(githubdata)
 })
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${port}`)
